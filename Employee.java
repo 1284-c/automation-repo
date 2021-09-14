5d2aec09d76bdf47d0ff140ab19a15c9caec0d8c
cerenaycoskun <54934812+1284-c@users.noreply.github.com>
2021-09-14 10:11:58 +0300

    Add files via upload
    I updated Employee.java file for getting salary amount from last homework and I changed weekly_Slary_Calculation file as method for using in Emolyee.java

--- Employee.java
+++ Employee.java
@@ -3,25 +3,40 @@ package week2;
 public class Employee {
     public int years;
     public void raise_amount() {
+    Weekly_Salary_Calculation  weekly_salary_calculation =new Weekly_Salary_Calculation();
+    double salary= weekly_salary_calculation.salary_calculation();
 
         if(years<1){
             System.out.println("No raise is made ");
+            salary=salary;
         }
         else{
             if(years==1){
-                System.out.println("Your raise %5 ");
+                salary=salary+ (salary*5)/100;
+                System.out.println("Your raise %5 "+salary);
             }
             else if(years==2){
-                System.out.println("Your raise %10 ");
+                salary=salary+ (salary*10)/100;
+                System.out.println("Your raise %10 "+salary);
+
+
             }
             else if(years==3){
-                System.out.println("Your raise %15 ");
+                salary=salary+ (salary*15)/100;
+                System.out.println("Your raise %15 "+salary);
+
             }
             else if(years==4){
-                System.out.println("Your raise %20 ");
+                salary=salary+ (salary*20)/100;
+                System.out.println("Your raise %20 "+salary);
+
+
             }
             else {
-                System.out.println("Your raise %25 ");
+                salary=salary+ (salary*25)/100;
+                System.out.println("Your raise %25 "+salary);
+
+
             }
         }
 

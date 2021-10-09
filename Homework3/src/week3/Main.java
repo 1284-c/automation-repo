ea8e81ea6485a7163a0c56fc39cad29618f3ee85
cerenaycoskun <54934812+1284-c@users.noreply.github.com>
2021-10-09 16:24:19 +0300

    HW3 for Simulator
    I try to make School simulator using constructor and OOP topics

---
+++ Homework3/src/week3/Main.java
@@ -0,0 +1,14 @@
+package week3;
+
+public class Main {
+
+    public static void main(String []args){
+        Student student =new Student("Cerenay", "Coskun",25,'K',2);
+        student.ShowPersonInfos();
+     //   Teacher teacher= new Teacher("Berivan","Kahraman",28, 'K');
+      //  teacher.ShowPersonInfos();
+
+
+        // Adding datas for simulator
+    }
+}
